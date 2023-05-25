49e3b45738b443139572da177ffe38fa60335cfc
strianeserenato <93558670+renstrianese@users.noreply.github.com>
2023-05-25 09:07:15 -0600

    First Push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace appservicedemo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
